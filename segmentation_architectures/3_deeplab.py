a43d539c87de8e9f45528dcebdec60f4f77a285f
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-12 02:07:18 -0400

    Create 3_deeplab.py

---
+++ segmentation_architectures/3_deeplab.py
@@ -0,0 +1,19 @@
+
+
+
+def convolution_block(block_input,num_filters=256,kernel_size=3,dilation_rate=1,padding='same',use_bias=False):
+
+
+
+
+
+def DilatedSpatialPyramidPooling(dspp_input):
+
+
+
+
+
+def deeplabv3plus():
+
+
+model = 

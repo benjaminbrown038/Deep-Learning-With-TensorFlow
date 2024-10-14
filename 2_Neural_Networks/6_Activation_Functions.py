c6c066531c3cf86a863c80f241dca1989b7797f1
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-14 09:23:57 -0400

    Update 6_Activation_Functions.py

--- 2_Neural_Networks/6_Activation_Functions.py
+++ 2_Neural_Networks/6_Activation_Functions.py
@@ -1,1 +1,36 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
 
+
+def plot_activation():
+
+    plt.close()
+
+
+def sigmoid():
+
+    return 
+
+
+def relu(x):
+
+    return
+
+
+def tanh(x):
+
+    return
+
+def elu(x):
+
+    return 
+
+
+def softmax(x):
+
+    return 
+
+def calculate_and_store_time():
+
+    

3d3e97950f04599c4c145f4afb9049bc6f589e8d
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-23 22:01:35 -0400

    Create 2_fcn32s_road_ce_loss.py

---
+++ 5-Semantic-Segmentation/2_fcn32s_road_ce_loss.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import glob as glob
+import albumentations as A
+import requests
+import zipfile
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Conv2D, Activation, Input, MaxPool2D, Conv2DTranspose
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from dataclasses import dataclass
+block_plot = False
+plt.rcParams['image.cmap'] = 'gray'
+
+def system_config():
+
+@dataclass(frozen=True)
+class DatasetConfig:
+
+@dataclass(frozen=True)
+class TrainingConfig:
+
+@dataclass(frozen=True)
+class InferenceConfig:
+
+def fcn32s_vgg16(num_classes,shape):
+    model_input = Input(shape=shape)
+
+    # convolution block 
+    x = Conv2D(64,(3,3),padding="same")(model_input)  
+    x = Activation("relu")(x)
+    x = Conv2D(64,(3,3),padding="same")(x)
+    x = Activation("relu")(x)
+    x = MaxPool2D((2,2),strides = (2,2))(x)
+
+    # convolution block 
+    x = Conv2D(128,(3,3),padding="same")(model_input)  
+    x = Activation("relu")(x)
+    x = Conv2D(128,(3,3),padding="same")(x)
+    x = Activation("relu")(x)
+    x = MaxPool2D((2,2),strides = (2,2))(x)
+
+    x = Conv2D(256,(3,3),padding="same")(model_input)  
+    x = Activation("relu")(x)
+    x = Conv2D(256,(3,3),padding="same")(x)
+    x = Activation("relu")(x)
+    x = Conv2D(256,(3,3),padding="same")(x)
+    x = Activation("relu")(x)
+    x = MaxPool2D((2,2),strides = (2,2))(x)
+
+    
+    
+
+model = fcn32s_vgg16()
+model.summary()
+
+def download_file():
+
+def unzip():
+
+save_name 
+
+class CustomSegDataLoader():
+    def __init__():
+    def __len__():
+    def transforms():
+    def resize():
+    def reset_array():
+    def __getitem__():
+
+def rgb_to_onehot():
+
+def num_to_rgb():
+
+def image_overlay():
+
+def display_image_and_mask():
+
+def create_datasets():
+
+model.compile()
+
+history = model.fit()
+
+def plot_results():
+
+
+    
